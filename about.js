4b47702d0fb946b9483b863824e4b0107c59a229
Isarel Getu <israelgetu101@gmail.com>
2022-02-25 11:33:51 -0600

    Part 2 Solved

--- about.js
+++ about.js
@@ -1,14 +1,33 @@
-console.log("hello world");
+// console.log("hello world");
 
+// function handleSubmit(evt) {
+//   evt.preventDefault();
 
+//   console.log("form submit");
+// }
 
-function handleSubmit(evt) {
-	evt.preventDefault();
-	
-	console.log('form submit');
+// let form = document.querySelector("form#contact");
+
+// form.addEventListener("submit", handleSubmit);
+
+function alertNotice(evt) {
+  evt.preventDefault();
+
+  alert("The form has been submitted successfully");
 }
 
+let form = document.querySelector("form#contact");
 
-let form = document.querySelector('form#contact');
+form.addEventListener("submit", alertNotice);
 
-form.addEventListener('submit', handleSubmit);
+
+
+
+let image = document.getElementById("#img");
+
+image.addEventListener("mouseover", function( event ) {
+
+  //event.target = "It is Working";
+	//console.log("It is Working")
+	alert("It is NOT Working")
+};
